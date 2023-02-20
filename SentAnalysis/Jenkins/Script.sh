190236293b8efe2ecad9a498d6bde25548f18aee
pawel <pawelbisok@gmail.com>
2023-02-20 15:27:37 +0100

    fixing script file

--- SentAnalysis/Jenkins/Script.sh
+++ SentAnalysis/Jenkins/Script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-sudo apt-get install -y \
-    apt-transport-htts \
-    ca-certificates \
-    curl \
-    software-properties-common
+apt-get install -y \
+apt-transport-htts \
+ca-certificates \
+curl \
+software-properties-common
 
 curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 
-sudo add-apt-repostiory \
+add-apt-repostiory \
     "deb https://apt.dockerproject.org/repo/ \
     ubuntu-$(lsb_release -cs) \
     main"
 
-sudo apt-get update
-sudo apt-get -y install docker-engine
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+apt-get update
+apt-get -y install docker-engine
+groupadd docker
+usermod -aG docker $USER
+systemctl enable docker
